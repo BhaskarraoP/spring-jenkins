49388c66c0f43e6d076c103aa27d0996badd2e64
Krishnarao Puppala <bhaskarrao.krishnarao-puppala@capgemini.com>
2023-07-11 00:39:38 +0530

    first commit

---
+++ src/main/java/com/example/jenkins/SpringJenkinsApplication.java
@@ -0,0 +1,26 @@
+package com.example.jenkins;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+	
+	public static Logger Logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+	
+	@PostConstruct
+	public void intt() {
+		Logger.info("Application Started...");
+	}
+
+	public static void main(String[] args) {
+		Logger.info("Application Executed...");
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+		Logger.info("Hey Bhaskar");
+	}
+
+}
